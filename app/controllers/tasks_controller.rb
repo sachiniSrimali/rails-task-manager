4fa1ad767b16c0cce74da92274b392d79b71b302
sachini weragodavidana <sachinisrimali@gmail.com>
2024-05-14 16:53:42 +0200

    Rails Task Manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+def task_params
+  params.require(:task).permit(:title, :details)
+end
+
+end
---
+++ db/migrate/20240514103019_upadte.rb
@@ -0,0 +1,5 @@
+class Upadte < ActiveRecord::Migration[7.1]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+  end
+end
